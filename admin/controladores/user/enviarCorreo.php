258035621c982a60ff50c6f154d522c7543b1185
nikoap77 <nikoap77@gmail.com>
2019-05-20 22:29:58 -0500

    Practica 4

--- admin/controladores/user/enviarCorreo.php
+++ admin/controladores/user/enviarCorreo.php
@@ -1,47 +1,52 @@
 <?php
-  session_start();
-  $codigo = $_SESSION['codigo'];
-  if(!isset($_SESSION['isLogged']) || $_SESSION['isLogged'] === FALSE){
-    header("Location: /SistemaDeGestion/public/vista/login.html");        
+session_start();
+$codigo = $_SESSION['codigo'];
+if (!isset($_SESSION['isLogged']) || $_SESSION['isLogged'] === FALSE) {
+    header("Location: /SistemaDeGestion/public/vista/login.html");
 }
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <title>Enviar Correo</title>
     <style type="text/css" rel="stylesheet">
     </style>
- </head>
+</head>
+
 <body>
 
     <?php
     //incluir conexión a la base de datos
-    include '../../../config/conexionBD.php';                
-    
-    $destinatario= isset($_POST["destinatario"]) ? trim($_POST["destinatario"]) : null;
-    $asunto= isset($_POST["asunto"]) ? trim($_POST["asunto"]) : null;    
-    $mensaje= isset($_POST["mensaje"]) ? trim($_POST["mensaje"]) : null;
-    
-    $sql ="SELECT usu_codigo FROM usuario WHERE usu_correo = '$destinatario'";
-    $result= $conn->query($sql);
-    $row= $result->fetch_assoc();
+    include '../../../config/conexionBD.php';
 
+    $destinatario = isset($_POST["destinatario"]) ? trim($_POST["destinatario"]) : null;
+    $asunto = isset($_POST["asunto"]) ? trim($_POST["asunto"]) : null;
+    $mensaje = isset($_POST["mensaje"]) ? trim($_POST["mensaje"]) : null;
+
+    $sql = "SELECT usu_codigo FROM usuario WHERE usu_correo = '$destinatario'";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
     $codDestinatario = $row['usu_codigo'];
-
-    $sql= "INSERT INTO correos VALUES(0, null, $codigo, $codDestinatario, '$asunto', '$mensaje', 'N', null)";        
     
-    if($conn->query($sql) === TRUE) {
-        echo"<p>Se ha enviado bien el mensaje!!!</p>";
-    } else{
-            echo"<p class='error'>Error: ". mysqli_error($conn) . "</p>";
-    }            
-        
+    $rol = $row['usu_rol'];
+
+    if ($rol == 'user') {
+        $sql = "INSERT INTO correos VALUES(0, null, $codigo, $codDestinatario, '$asunto', '$mensaje', 'N', null)";
+        if ($conn->query($sql) === TRUE) {
+            echo "<p>Se ha enviado bien el mensaje!!!</p>";
+        } else {
+            echo "<p class='error'>Error: " . mysqli_error($conn) . "</p>";
+        }
+    } else {
+        echo "No se puede enviar un Correo a un usuario ADMIN";
+    }
     //cerrar la base de datos
     $conn->close();
-    echo"<a href='../../vista/user/index.php'>Regresar</a>";
- ?>
+    echo "<a href='../../vista/user/index.php'>Regresar</a>";
+    ?>
 
 </body>
 <html>
